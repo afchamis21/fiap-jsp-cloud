9d963fb5ea0acd9969f34f93dd1273e3a9adea32
André <andrechamis21@gmail.com>
2025-04-07 22:18:39 -0300

    Config pra rota "/" mapear pra "/home"

---
+++ src/main/java/com/fiap/on/esoa/controllers/HomeServlet.java
@@ -0,0 +1,19 @@
+package com.fiap.on.esoa.controllers;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/home")
+public class HomeServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("Acessando o servlet...");
+        // TODO
+        req.getRequestDispatcher("/home.jsp").forward(req, resp);
+    }
+}
---
+++ src/main/webapp/home.jsp
@@ -0,0 +1,20 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: afcha
+  Date: 07/04/2025
+  Time: 22:09
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Vinheria Agnello</title>
+</head>
+<body>
+<%
+    String message = "Hello World";
+%>
+
+<h2><%=message%></h2>
+</body>
+</html>
