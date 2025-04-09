d4af53f4a509038e1457ad0dc1aebe2d250bffbc
André <andrechamis21@gmail.com>
2025-04-09 20:44:05 -0300

    Conexão incial com db e pequena refatoração do Wine.java

---
+++ src/main/java/com/fiap/on/esoa/dao/BaseDao.java
@@ -0,0 +1,25 @@
+package com.fiap.on.esoa.dao;
+
+import com.fiap.on.esoa.env.EnvLoader;
+
+import java.sql.*;
+
+public abstract class BaseDao {
+    protected Connection getConnection() throws SQLException {
+        String user = EnvLoader.getInstance().getValue("AZURE_DB_LOGIN")
+                .orElseThrow(() -> new RuntimeException("Azure DB Login is missing!"));
+
+        String db = EnvLoader.getInstance().getValue("AZURE_DB_NAME")
+                .orElseThrow(() -> new RuntimeException("Azure DB Name is missing!"));
+
+        String password = EnvLoader.getInstance().getValue("AZURE_DB_PWD")
+                .orElseThrow(() -> new RuntimeException("Azure DB Password is missing!"));
+
+        String conn = "jdbc:sqlserver://fiap-on-esoa3-agnello.database.windows.net:1433;database=vinheria_agnello;user={user}@{db_name};password={password};encrypt=true;trustServerCertificate=false;hostNameInCertificate=*.database.windows.net;loginTimeout=30;"
+                .replace("{user}", user)
+                .replace("{db_name}", db)
+                .replace("{password}", password);
+
+        return DriverManager.getConnection(conn);
+    }
+}
--- src/main/java/com/fiap/on/esoa/domain/Wine.java
+++ src/main/java/com/fiap/on/esoa/domain/Wine.java
@@ -3,15 +3,15 @@ package com.fiap.on.esoa.domain;
 import java.math.BigDecimal;
 
 public class Wine {
-    private final String maker;
-    private final String name;
-    private final String image;
-    private final String alt;
-    private final String country;
-    private final String grape;
-    private final BigDecimal price;
-    private final Integer rating;
-    private final Integer year;
+    private String maker;
+    private String name;
+    private String image;
+    private String alt;
+    private String country;
+    private String grape;
+    private BigDecimal price;
+    private Integer rating;
+    private Integer year;
 
     public Wine(String maker, String name, String image, String alt, String country, String grape, BigDecimal price, Integer rating, Integer year) {
         this.maker = maker;
@@ -25,39 +25,93 @@ public class Wine {
         this.year = year;
     }
 
+    public Wine() {
+    }
+
     public String getMaker() {
         return maker;
+    }
+
+    public void setMaker(String maker) {
+        this.maker = maker;
     }
 
     public String getName() {
         return name;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public String getImage() {
         return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
     }
 
     public String getAlt() {
         return alt;
     }
 
+    public void setAlt(String alt) {
+        this.alt = alt;
+    }
+
     public String getCountry() {
         return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getGrape() {
+        return grape;
+    }
+
+    public void setGrape(String grape) {
+        this.grape = grape;
     }
 
     public BigDecimal getPrice() {
         return price;
     }
 
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
     public Integer getRating() {
         return rating;
+    }
+
+    public void setRating(Integer rating) {
+        this.rating = rating;
     }
 
     public Integer getYear() {
         return year;
     }
 
-    public String getGrape() {
-        return grape;
+    public void setYear(Integer year) {
+        this.year = year;
+    }
+
+    @Override
+    public String toString() {
+        return "Wine{" +
+                "maker='" + maker + '\'' +
+                ", name='" + name + '\'' +
+                ", image='" + image + '\'' +
+                ", alt='" + alt + '\'' +
+                ", country='" + country + '\'' +
+                ", grape='" + grape + '\'' +
+                ", price=" + price +
+                ", rating=" + rating +
+                ", year=" + year +
+                '}';
     }
 }
